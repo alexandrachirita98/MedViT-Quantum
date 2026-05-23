9730dc79a3867e41c22f813c06b5ff0e33566d7a
Alexandra Chirita <daachirita@gmail.com>
2026-05-23 21:25:22 +0300

    mhca only bug fix

--- quantum_variants/__init__.py
+++ quantum_variants/__init__.py
@@ -1,10 +1,8 @@
 from quantum_variants.softmax_only import Q_E_MHSA, QLTB, QMedViT_Softmax_Only
-from quantum_variants.e_mhca_only import Q_MHCA, QMedViT_E_MHCA_Only
 from quantum_variants.quanv_stem import QuanvStem, QMedViT_Quanv_Stem
 
 VARIANTS = {
     "softmax": QMedViT_Softmax_Only,
-    "emhca": QMedViT_E_MHCA_Only,
     "quanv_stem": QMedViT_Quanv_Stem,
 }
 
@@ -12,8 +10,6 @@ __all__ = [
     "Q_E_MHSA",
     "QLTB",
     "QMedViT_Softmax_Only",
-    "Q_MHCA",
-    "QMedViT_E_MHCA_Only",
     "QuanvStem",
     "QMedViT_Quanv_Stem",
     "VARIANTS",
